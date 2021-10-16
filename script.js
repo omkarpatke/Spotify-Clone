ab4ca3171637439d4032b12f0f56a4ef353a01ec
omkar patke <omkarpatke1101@gmail.com>
2021-10-16 13:30:10 +0530

    fix: initial song status

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // initialise a variables
 let songIndex = 0;
-let audioElement = new Audio('songs/Chak_Lein_De.mp3');
+let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar =  document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
